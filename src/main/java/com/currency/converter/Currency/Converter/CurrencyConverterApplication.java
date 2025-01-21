ad3b40146837e8ec733308aa7326510a2c303c77
saifuddin khan <109955734+khansaif1023@users.noreply.github.com>
2025-01-21 20:30:28 +0530

    Inital commit

---
+++ src/main/java/com/currency/converter/Currency/Converter/CurrencyConverterApplication.java
@@ -0,0 +1,14 @@
+package com.currency.converter.Currency.Converter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurrencyConverterApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(CurrencyConverterApplication.class, args);
+	}
+
+}
